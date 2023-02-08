adeed3c712ba82decb79d13e3ac5399a63d723bf
MONLEMON <90751057+MONLEMON@users.noreply.github.com>
2023-02-08 23:54:07 +0700

    Add files via upload

---
+++ Project/src/UPBEAT.java
@@ -0,0 +1,29 @@
+public abstract class UPBEAT {
+    boolean owned;
+    int rows;
+    int cols;
+    int money;
+    void Pos(){
+        System.out.println("Position is row" + rows + "cols" + cols);
+    }
+    void budget(){
+        System.out.println("Money remain" + money);
+    }
+    int deposit(int row,int col){
+        if(owned){
+            return -1;
+        }else return 1;
+    }
+
+    class City{
+    }
+    class CityCenter extends City{
+    }
+    class Player{
+
+    }
+    class Opponent{
+    }
+    class OpPos extends Opponent{
+    }
+}
