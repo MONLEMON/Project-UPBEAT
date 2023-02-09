0255781330f2848eba7f7a25be63b068539afe18
Surabordin <bluelimec@gmail.com>
2023-02-09 22:10:11 +0700

    update

--- Project/src/UPBEAT.java
+++ Project/src/UPBEAT.java
@@ -1,10 +1,23 @@
 public  class UPBEAT {
+ /**ConfigurationFile**/
+    int m=20;
+    int n=15;
+    int init_plan_min=5;
+    int init_plan_sec=0;
+    int init_budget=10000;
+    int init_center_dep=100;
+    int plan_rev_min=30;
+    int plan_rev_sec=0;
+    int rev_cost=100;
+    int max_dep=1000000;
+    int interest_pct=5;
+  /**-----------------**/
     boolean owned;
-    int rows;
-    int cols;
-    int money;
-    void Pos(){
-        System.out.println("Position is row" + rows + "cols" + cols);
+    String rows(int m){
+        return "Have "+m+" rows in territory";
+    }
+    String cols(int n){
+        return "Have "+n+" rows in territory";
     }
     void budget(){
         System.out.println("Money remain" + money);
@@ -14,7 +27,7 @@ public  class UPBEAT {
             return -1;
         }else return 1;
     }
-    class ActionCommands {
+    class ActionCommands extends UPBEAT {
         void done(){
         }
         void relocate(){
@@ -28,10 +41,25 @@ public  class UPBEAT {
         void shoot(){
         }
     }
-    class City{
+    class ConfigurationFile {
+
+    }
+    class City extends UPBEAT{
+
     }
     class CityCenter extends City{
     }
-    class Player{
+    class Player extends UPBEAT{
+        int row;
+        int col;
+        String currows(){
+            return "City crew in"+row+"row";
+        }
+        String curcols(){
+            return "City crew in"+col+"col";
+        }
+        String Pos(){
+            return "Position is row" + row + "cols" + col;
+        }
     }
 }
